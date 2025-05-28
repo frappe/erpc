f86216a6af22f6002ccf92ef64d5d2233b7479a2
Ankush Menat <ankush@frappe.io>
2025-05-28 10:28:48 +0530

    fix: use vu argument for setup
    quick setup during testing instead of all N users

--- src/script.js
+++ src/script.js
@@ -5,23 +5,25 @@ const BASE_URL = "http://erpc.localhost:8000";
 const NUM_ITEMS = 10000;
 const NUM_WAREHOUSES = 10;
 const NUM_CUSTOMERS = 3000 * NUM_WAREHOUSES;
-const NUM_USERS = 1 * NUM_WAREHOUSES;
+const NUM_USERS = 10 * NUM_WAREHOUSES;
 const COMPANY = "The Company";
+
+export const options = {
+	vus: NUM_USERS,
+};
 
 export function setup() {
 	// login every user and remember credentials
 	// master data is created by python setup counterpart
 
 	const sids = [];
-	Array.from(Array(NUM_USERS).keys())
+	Array.from(Array(options.vus).keys())
 		.map((i) => `u-${String(i + 1).padStart(4, "0")}@erpc.local`) // TODO: provide list?
 		.forEach((username) => {
 			let res = http.post(`${BASE_URL}/api/method/login`, { usr: username, pwd: username });
-			check(res, {
-				"login succesful": (r) => r.status == 200,
-				"sid present in response": (r) => r.cookies.sid?.[0]?.value != null,
-			});
-
+			if (r.status != 200) {
+				throw new Error(`User login failed for ${username}`);
+			}
 			sids.push(res.cookies.sid[0].value);
 		});
 
